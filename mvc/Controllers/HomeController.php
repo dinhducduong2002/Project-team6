548099a8337dfd21e295c66b7fde421a31ab92a2
lucvcph14671 <94091979+lucvcph14671@users.noreply.github.com>
2021-11-15 21:15:32 +0700

    Update HomeController.php

--- mvc/Controllers/HomeController.php
+++ mvc/Controllers/HomeController.php
@@ -2,23 +2,27 @@
 class HomeController extends Controller{
     public $GetProduct;
     public $GetDetailProduct;
+
     public function __construct()
     {
         $this->GetProduct = $this->model("ProductModel");
         $this->GetDetailProduct = $this->model("ProductModel");
     }
+
     public function Home(){
 
         $this->view("HomeViews", [
             "pages" => "HomePage"
         ]);
     }
+
     public function ShowProduct(){
         $this->view("ProductViews", [
             "pages" => "ProductPage",
             "Product"=>$this-> GetProduct->GetProduct(),
         ]);
     }
+
     public function DetailProduct($id){
         $this->view("ProductViews", [
             "pages" => "DetailPage",
@@ -26,11 +30,19 @@ class HomeController extends Controller{
             "DetailProduct"=>$this-> GetDetailProduct->GetDetailProduct($id)
         ]);
     }
+
+    public function pay(){
+        $this->view("ProductViews", [
+            "pages" => "login"
+        ]);
+    }
+
     public function login(){
         $this->view("ProductViews", [
             "pages" => "login"
         ]);
     }
+
     public function register(){
         $this->view("ProductViews", [
             "pages" => "register"
