5d3045e6d42113e86b7f396a2bdae4345f24fc68
lucvcph14671 <94091979+lucvcph14671@users.noreply.github.com>
2021-11-19 22:46:58 +0700

    thanh toán

---
+++ business/pay.php
@@ -0,0 +1,17 @@
+<?php
+function pay(){
+
+    $sql_data_pay = "SELECT * FROM account WHERE id=1";
+    $data_pay = executeQuery($sql_data_pay);
+
+    $id = $_GET['id'];
+    $sql_data_products = "SELECT * FROM products WHERE id='$id'";
+    $sql_data_products = executeQuery($sql_data_products);
+
+    client_render('product/pay.php',[
+
+        'data_pay' => $data_pay,
+        'sql_data_products' => $sql_data_products,
+    ]
+    );
+}
