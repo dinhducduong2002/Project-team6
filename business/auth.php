c13240c18f6f3436683756eb6557aed308363e7e
duyph15719 <duyncph15719@fpt.edu.vn>
2021-11-22 18:03:26 +0700

    1

--- business/auth.php
+++ business/auth.php
@@ -1,5 +1,9 @@
 <?php
 function login(){
+    $username = $_GET['username'];
+    $password =$_GET['password'];
+    $sql="select * from users where username = '$username' and password = '$password'";
+    $user = executeQuery($sql);
             client_render('/login.php');
 }
     
