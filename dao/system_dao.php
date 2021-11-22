e1c8026d5de8e5c9608755cc5f759ab00ba503fa
duyph15719 <duyncph15719@fpt.edu.vn>
2021-11-22 16:48:55 +0700

    delete
    delete

--- dao/system_dao.php
+++ dao/system_dao.php
@@ -18,14 +18,5 @@ function executeQuery($sql, $getAll = true){
         }
     }
 }
-function input_post($key){
-    return isset($_POST[$key]) ? trim($_POST[$key]) : false;
-}
-function is_submit($key){
-    return (isset($_POST['request_name']) && $_POST['request_name'] == $key);
-}
-function show_error($error, $key){
-    echo '<span style="color: red">'.(empty($error[$key]) ? "" : $error[$key]). '</span>';
-}
 
 ?>
--- views/homepage/login.php
+++ views/homepage/login.php
@@ -37,53 +37,6 @@
 </head>
 
 <body>
-    <?php 
-		$error = array(); 
-
-// BƯỚC 2: NẾU NGƯỜI DÙNG SUBMIT FORM
-    if (is_submit('login'))
-    {    
-    // lấy tên đăng nhập và mật khẩu
-    $username = input_post('username');
-    $password = input_post('password');
-     
-    // Kiểm tra tên đăng nhập
-    if (empty($username)){
-        $error['username'] = 'Bạn chưa nhập tên đăng nhập';
-    }
-     
-    // Kiểm tra mật khẩu
-    if (empty($password)){
-        $error['password'] = 'Bạn chưa nhập mật khẩu';
-    }
-     
-    // Nếu không có lỗi
-    if (!$error)
-    {
-        // include file xử lý database user
-        include_once('dao/user.php');
-         
-        // lấy thông tin user theo username
-        $user = db_user_get_by_username($username);
-         
-        // Nếu không có kết quả
-        if (empty($user)){
-            $error['username'] = 'Tên đăng nhập không đúng';
-        }
-        // nếu có kết quả nhưng sai mật khẩu
-        else if ($user['password'] != md5($password)){
-            $error['password'] = 'Mật khẩu bạn nhập không đúng';
-        }
-         
-        // nếu mọi thứ ok thì tức là đăng nhập thành công 
-        // nên thực hiện redirect sang trang chủ
-        if (!$error){
-            set_logged($user['username'], $user['level']);
-            redirect(base_url('admin/?m=common&a=dashboard'));
-        }
-    }
-    }
-    ?>
     <!-- /header -->
     <!-- /banner -->
     <main>
@@ -94,7 +47,6 @@
                         <span class="login100-form-title p-b-49">
                             Đăng Nhập
                         </span>
-                        <?php show_error($error, 'username'); ?>
                         <div class="wrap-input100 validate-input m-b-23" data-validate="Tài Khoản Không Đúng">
                             <span class="label-input100">Tài Khoản</span>
                             <input class="input100" type="text" name="username" placeholder="Tài Khoản Của Web">
