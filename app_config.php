a7bbb3a49041d7679e472e1d825307aaf663876b
duyph15719 <duyncph15719@fpt.edu.vn>
2021-11-24 11:27:31 +0700

    up lại

---
+++ app_config.php
@@ -0,0 +1,12 @@
+<?php
+
+const BASE_URL = "http://localhost/Project-team6/";
+
+const ADMIN_URL = "http://localhost/Project-team6/cp-admin/";
+const CLIENT_URL = "http://localhost/Project-team6/";
+const ADMIN_ASSETS = BASE_URL . 'public/adminlte/';
+const CLIENT_ASSETS = BASE_URL . 'public/client/';
+const PUBLIC_ASSETS = "http://localhost/Project-team6/public/";
+
+
+?>
---
+++ business/pay.php
@@ -0,0 +1,22 @@
+<?php
+function pay(){
+    $sql_data_pay = "SELECT * FROM account WHERE username='".$_SESSION['client']."'";
+    $data_pay = executeQuery($sql_data_pay);
+
+    $id = $_GET['id'];
+    $sql_data_products = "SELECT * FROM products WHERE id='$id'";
+    $sql_data_products = executeQuery($sql_data_products);
+
+    client_render('product/pay.php',[
+
+        'data_pay' => $data_pay,
+        'sql_data_products' => $sql_data_products,
+    ]
+    );
+
+    if(isset($_POST['btnAdd'])){
+        $username = $_SESSION['client'];
+        $sql = "UPDATE account_purchase_history SET name_account='$username' WHERE $username='".$_SESSION['client']."'";
+        executeQuery($sql);
+    }
+}
