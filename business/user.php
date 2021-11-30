d77fe6658771b7c03b970e2ce2d808ace48e7ae5
duyph15719 <duyncph15719@fpt.edu.vn>
2021-11-30 11:52:32 +0700

    up đổi mk

--- business/user.php
+++ business/user.php
@@ -12,5 +12,42 @@ function history(){
     client_render('user/history.php', ['aph' => $aph]);
 }
 function change_password(){
+    if(isset($_POST['btnSub'])){
+        $username=$_SESSION['user']['username'];
+        $password = $_POST['password'];
+        $newpassword = $_POST['newpassword'];
+        $passwordHash = password_hash($newpassword, PASSWORD_DEFAULT);
+        $confirmpassword = $_POST['confirmpassword'];
+            
+        if (empty($password)) {
+            $_SESSION['password'] = "Bạn cần nhập vào mật khẩu cũ";
+        }else if (strlen($password) < 6) {
+            $_SESSION['password'] = 'Mật khẩu tối thiểu 6 ký tự';
+        }
+        if (empty($newpassword)) {
+            $_SESSION['newpassword'] = 'Bạn cần nhập vào mật khẩu mới';
+        }else if (strlen($newpassword) < 6) {
+            $_SESSION['newpassword'] = 'Mật khẩu tối thiểu 6 ký tự';
+        }
+        if (empty($confirmpassword)) {
+            $_SESSION['confirmpassword'] = 'Bạn cần nhập lại mật khẩu mới';
+        }else if ($confirmpassword != $newpassword) {
+            $_SESSION['confirmpassword'] = 'Xác nhận mật khẩu không khớp';
+        }
+        $sql="SELECT * FROM account where id = '".$_SESSION['user']['id']."' ";
+            $check_user = executeQuery($sql);
+            if($check_user){
+                if (password_verify($password, $check_user[0]['password']) != $password) {
+                $_SESSION['password'] = "Bạn nhập sai mật khẩu";
+              }
+              else {   
+                $sql = "UPDATE account SET password='$passwordHash' where id = '".$_SESSION['user']['id']."' ";
+                executeQuery($sql);
+                $_SESSION['success'] = "Đổi mật khẩu thành công";
+                header("location: " . CLIENT_URL . 'user/change-password');
+            }
+            }
+        
+        }  
     client_render('user/change-password.php');
 }
--- views/homepage/user/change-password.php
+++ views/homepage/user/change-password.php
@@ -16,6 +16,56 @@
                             <li><a href="<?= CLIENT_URL ?>user/history" class="">Lịch sử giao dịch</a></li>
                         </ul>
                     </div>
+                <form style="width:50%; margin:auto;" class="login100-form validate-form" method="post">
+                <span class="login100-form-title p-b-30">
+                    Đổi Mật Khẩu
+                </span>
+                <?php if (isset($_SESSION['success'])) : ?>
+                    <div  role="alert">
+                    <?php echo $_SESSION['success'];
+                        unset($_SESSION['success']); ?>
+                    </div>
+                <?php endif; ?>
+                <div class="wrap-input100 m-b-23">
+                    <span class="label-input100"  >Tài Khoản :</span>  <?php echo($_SESSION['user']['username']);?>
+                </div>
+
+                <div class="wrap-input100 ">
+                    <span class="label-input100">Mật Khẩu Cũ</span>
+                    <input class="input100" type="password" name="password" placeholder="Mật Khẩu"
+                        value="<?php if(isset($password)){echo $password;}?>">
+                    <span class="focus-input100" data-symbol="&#xf190;"></span>
+                    <span
+                        style="color: red;"><?php if (isset($_SESSION['password'])) {echo $_SESSION['password'];}unset($_SESSION['password']) ?></span>
+                </div>
+
+                <div class="wrap-input100 ">
+                    <span class="label-input100">Mật Khẩu Mới</span>
+                    <input class="input100" type="password" name="newpassword" placeholder="Mật Khẩu"
+                        value="<?php if(isset($newpassword)){echo $newpassword;}?>">
+                    <span class="focus-input100" data-symbol="&#xf190;"></span>
+                    <span
+                        style="color: red;"><?php if (isset($_SESSION['newpassword'])) {echo $_SESSION['newpassword'];}unset($_SESSION['newpassword']) ?></span>
+                </div>
+
+                <div class="wrap-input100 ">
+                    <span class="label-input100">Nhập Lại Mật Khẩu Mới</span>
+                    <input class="input100" type="password" name="confirmpassword" placeholder="Mật Khẩu"
+                        value="<?php if(isset($confirmpassword)){echo $confirmpassword;}?>">
+                    <span class="focus-input100" data-symbol="&#xf190;"></span>
+                    <span
+                        style="color: red;"><?php if (isset($_SESSION['confirmpassword'])) {echo $_SESSION['confirmpassword'];}unset($_SESSION['confirmpassword']) ?></span>
+                </div>
+                    <br>
+                <div class="container-login100-form-btn">
+                    <div class="wrap-login100-form-btn">
+                        <div class="login100-form-bgbtn"></div>
+                        <button class="login100-form-btn" type="submit" name="btnSub">
+                            Cập Nhập
+                        </button>
+                    </div>
+                </div>
+            </form>
                 </div>
             </div>
         </div>
